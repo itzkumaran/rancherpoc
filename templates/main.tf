7d8cb69e2d73cacc93c247c6f2f5e85c997a8cfa
itzkumaran <45903786+itzkumaran@users.noreply.github.com>
2018-12-27 12:06:59 -0500

    Update main.tf

--- templates/main.tf
+++ templates/main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 
 module "rancher" {
   source                 = "../modules/rancher"
-  instance_count         = "2"
-  availability_zones     = ["us-east-1a","us-east-1b","us-east-1c"]
+  instance_count         = "3"
+  availability_zones     = ["us-east-1c","us-east-1d","us-east-1e"]
   vpc_id                 = "vpc-505b032b"
   instance_type          = "t2.medium"
   ami_id                 = "ami-0ac019f4fcb7cb7e6"
